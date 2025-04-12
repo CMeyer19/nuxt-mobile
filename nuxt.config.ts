fa14fb8334b60a27931d14f0051ea552d8520dd6
CMeyer19 <colemeyer95@outlook.com>
2025-04-12 14:17:28 +0200

    Adding initial project

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: {enabled: true},
+  modules: ['@nuxt/eslint', '@nuxtjs/ionic'],
+  ssr: false,
+  ionic: {
+    integrations: {
+      //
+    },
+    css: {
+      //
+    },
+    config: {
+      //
+    }
+  },
+})
